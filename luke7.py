a4e9a1bde682775c718d1528808bdd4c449c2a5e
Maximiliano Horta <maximilianohorta94@gmail.com>
2016-12-08 01:05:49 +0100

    ferdig med luke7

---
+++ luke7.py
@@ -0,0 +1,21 @@
+def day7():
+    f = open("luke7_input.txt")
+    lines = f.readlines()
+    x=0
+    y=0
+    for line in lines:
+        splitted = line.strip().split(" ")
+        a=int(splitted[1])
+        if splitted[3] == "north":
+            y += a
+        elif splitted[3] == "east":
+            x -= a
+        elif splitted[3] == "south":
+            y -= a
+        elif splitted[3] == "west":
+            x += a
+    return x,y
+
+x,y = day7()
+
+print y,x
